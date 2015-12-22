82592696cd0ac624bb067818598d51560382d273
Alan Gilbert <agilbert@deliveryagent.com>
2015-12-22 15:40:35 -0500

    solved p18

--- src/P18.hs
+++ src/P18.hs
@@ -12,14 +12,28 @@ idxes = [fst bounds .. snd bounds]
 
 p18 = do
   arr <- createP18Array
-  val <- liveNeighbors arr (10,50)
-  print val
+  let gens = repeat (getNextGen arr)
+  sequence_ (take 100 gens)
+  return arr
 
 createP18Array = do
   initState <- lines <$> slurp "18.txt"
   arr <- newArray ((fst bounds,fst bounds), (snd bounds, snd bounds)) False :: IO LifeArray
-  initializeArray arr $ zip [0..] initState
+  initializeArray arr $ zip [0..] (translateInput initState)
   return arr
+  where
+    translateInput grid = map (map (\v -> if v == '#' then True else False)) grid
+
+countLiveCells :: LifeArray -> IO Int
+countLiveCells arr = do
+  cells <- mapM (readArray arr) [(x,y) | x <- idxes, y <- idxes]
+  return $ length (filter id cells)
+
+getNextGen :: LifeArray -> IO ()
+getNextGen arr = do
+  next <- mapM (rowFate arr) idxes
+  initializeArray arr (zip [0..] next)
+  mapM_ (\p -> writeArray arr p True) [(0,0),(0,99),(99,0),(99,99)]
 
 rowFate :: LifeArray -> Int -> IO [Bool]
 rowFate arr r = mapM (\c -> cellFate arr (r,c)) idxes
@@ -48,6 +62,6 @@ isBetween (l,h) t = l <= t && t <= h
 
 initializeArray arr is = mapM_ (initializeRow arr) is
 
-initializeRow :: IOArray (Int, Int) Bool -> (Int,[Char]) -> IO ()
+initializeRow :: LifeArray -> (Int,[Bool]) -> IO ()
 initializeRow arr (r,cs) =
-  mapM_ (\(c,v) -> writeArray arr (r,c) (if v == '#' then True else False)) (zip [0..] cs)
+  mapM_ (\(c,v) -> writeArray arr (r,c) v) (zip [0..] cs)
