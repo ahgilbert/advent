b2095ecd3dae717b62fa161f7639a8bd987560b2
Alan Gilbert <agilbert@deliveryagent.com>
2015-12-23 17:48:27 -0500

    deleting unused p19 stuff

--- src/P19.hs
+++ src/P19.hs
@@ -6,15 +6,12 @@ import Data.List
 import Data.Tree
 import Text.Megaparsec
 
-data Faith = Faith [(Char, [Faith])]
-type Molecule = String
-
 -- Problem 19 --
 p19 = do
   inp <- slurp "19.txt"
   let (swaps, seed) = head $ rights [runParser parseMedicine "" inp]
       molecules = nub $ faith swaps 1 "" seed
-    in print $ (molecules, length molecules)
+    in print $ ("", length molecules)
 
 groupSwaps swaps = let
   sorted = sortBy (\a b -> compare (fst a) (fst b)) swaps
@@ -34,39 +31,6 @@ faith rules tick sofar seed =
                       matches
       skipThis = faith rules tick (sofar ++ [head seed]) (drop 1 seed)
   in continuations ++ skipThis
-
-genSomeMolecule :: (Molecule, Molecule) -> [Molecule] -> Molecule
-genSomeMolecule swaps seed =
-  let faith = findMatches swaps seed
-      hope = map (genSomeMolecule swaps) (applyMatches seed)
-  in "ahg"
-
-findMatches = undefined
-applyMatches = undefined
-
-walkInput :: (Eq a) => [([a],[a])] -> [a] -> Int
-walkInput _ [] = 0
-walkInput allRules is@(f:rs) = let
-  rules = filter (\(k,_) -> isPrefixOf k is) allRules
-  in length rules + walkInput allRules rs
-
-{-
-HOH : H OH [H>OH, H>HO, O>HH]
-          [(OH, OH), (HO, OH)]
-      O H [H>OH, H>HO, O>HH]
-          [(HH,H)]
-      H _ [H>OH, H>HO, O>HH]
-
--}
-
-
-
-
-
-
-
-
-
 
 
 -- Parsers --
