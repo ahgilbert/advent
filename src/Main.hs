b89d563f9eaa8eeacc0704c62f43b3900375b153
Alan Gilbert <agilbert@deliveryagent.com>
2015-12-26 13:00:27 -0500

    passably performant 19_2

--- src/Main.hs
+++ src/Main.hs
@@ -24,8 +24,19 @@ import P20
 import P24
 import P25
 
+import Data.List
+
 quickProblems = [p1_1, p1_2, p2, p3_1, p3_2, p5_1, p5_2, p7, p8_1, p8_2, p9, p10, p11]
 slowProblems = [p4, p6_1, p18]
 
 main :: IO ()
-main = test25 3010 3019
+main = do
+  (swaps, seed) <- slurp19
+  let faith = iterate (p19Cycle swaps) [seed]
+      hope = dropWhile (\xs -> minimum (map length xs) > 10) faith
+  mapM_ print $ zip (head hope) (map length (head hope))
+
+-- run synth, yield the 10 most promising results
+p19Cycle :: [(String, String)] -> [String] -> [String]
+p19Cycle swaps seeds =
+  nub $ take 50 $ sortOn length $ concatMap (synth swaps 1 "") seeds
