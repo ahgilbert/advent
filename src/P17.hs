82527db5ea7147eedbd17a626653563e97ea1030
Alan Gilbert <agilbert@deliveryagent.com>
2016-01-06 12:02:56 -0500

    print both solutions to p 17

--- src/P17.hs
+++ src/P17.hs
@@ -5,7 +5,12 @@ import Data.List
 
 p17 = do
   scoops <- (reverse . sort) <$> slurpLinesWith parseInt "17.txt"
-  return $ getCombosSumming 150 scoops
+  let allCombos = getCombosSumming 150 scoops
+      part1 = length allCombos
+      minN = minimum $ map length allCombos
+      part2 = length $ filter (\x -> length x == minN) allCombos
+  print part1
+  print part2
 
 getCombosSumming target (n:ns)
   | target == n = [[n]] ++ getCombosSumming target ns
