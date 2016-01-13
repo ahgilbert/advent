6d0c1b49b3a2060027f65028a4c00175de531ab9
Alan Gilbert <alanhgilbert@gmail.com>
2016-01-13 04:52:36 +0200

    solve P20_1

--- src/P20.hs
+++ src/P20.hs
@@ -1,10 +1,31 @@
 module P20 where
 
+import Data.List
 import P17
 
--- target = 34000000
+--target = 34000000
 
-p20 = do
-  print $ "ahg"
+p20 target = do
+  let factors = map factorize [1..]
+      qualifying = dropWhile (\fs -> sum fs < target) factors
+    in print $ head qualifying
 
-getSummands n = getCombosSumming n (reverse [1..n])
+factorize n =
+  nub $ [1, n] ++ factorize' n 2
+
+factorize' target current
+  | current * current > target = []
+  | True = let
+    (q,r) = quotRem target current
+    in if r == 0
+       then [current, q] ++ factorize' target (current + 1)
+       else factorize' target (current + 1)
+
+factorize2 n =
+  nub $ factorize2' n 1
+
+factorize2' target current
+  | current * current > target = []
+  | True = let
+    (q,r) = quotRem target current
+    in undefined
